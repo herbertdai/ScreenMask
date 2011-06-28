03d87526f5fc284f480a587099ef22f341035dcd
zhihu ding <dingzhihu@gmail.com>
2011-06-28 14:31:38 +0800

    test 4 transparent activity

--- src/com/howfun/android/screenmask/MainPage.java
+++ src/com/howfun/android/screenmask/MainPage.java
@@ -2,12 +2,40 @@ package com.howfun.android.screenmask;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
+import android.view.Window;
+import android.view.View.OnClickListener;
+import android.widget.Button;
 
 public class MainPage extends Activity {
-    /** Called when the activity is first created. */
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.main);
-    }
+   /** Called when the activity is first created. */
+   
+   public static final String TAG = "ScreenMask";
+
+   Button mBtn4Test = null;
+
+   @Override
+   public void onCreate(Bundle savedInstanceState) {
+      requestWindowFeature(Window.FEATURE_NO_TITLE);
+      super.onCreate(savedInstanceState);
+      setContentView(R.layout.main);
+      findViews();
+      setupListeners();
+   }
+
+   private void findViews() {
+      mBtn4Test = (Button) findViewById(R.id.button4test);
+   }
+
+   private void setupListeners() {
+      if(mBtn4Test != null){
+         mBtn4Test.setOnClickListener(new OnClickListener() {
+            
+            @Override
+            public void onClick(View v) {
+               
+            }
+         });
+      }
+   }
 }
